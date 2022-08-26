b4743114a9dba84e54a3f22b524e36d8a44631c1
abderox <abdelhadi12mouzafir@gmail.com>
2022-08-26 23:04:18 +0100

    STAGE-PFA-FRONEND : FIXING cors problems with spring and react

--- src/api/auth/auth.api.js
+++ src/api/auth/auth.api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import * as type from '../../utils/constants';
 import authHeader from "./auth-header";
-const URL =type.default;
+const URL = type.default;
 
 const login = (email, password, url) => {
     return axios
@@ -13,7 +13,7 @@ const login = (email, password, url) => {
             console.log("🚀 ~ file: auth.api.js ~ line 11 ~ .then ~ response", response)
             if (response.data) {
                 localStorage.setItem("adria-user", JSON.stringify(response.data));
-                
+
             }
             return response.data;
         }
@@ -22,13 +22,8 @@ const login = (email, password, url) => {
 
 
 const logout = () => {
-    
-    // getData(URL.API_URL_V2 + URL.SIGN_OUT_URL_ADMIN);
-    
-            // localStorage.removeItem("adria-user");
 
-
-    axios.get(URL.API_URL_V2 + URL.SIGN_OUT_URL_ADMIN,  authHeader() ).then(response => {
+    axios.get(URL.API_URL_V2 + URL.SIGN_OUT_URL_ADMIN, authHeader()).then(response => {
         if (response.status === 200) {
             localStorage.removeItem("adria-user");
         }
